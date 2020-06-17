8441bf8a3845cb3084784623278111c18d7b1b81
Coty Condry <ccondry@cisco.com>
2020-06-17 13:01:36 -0500

    return a nice status message and the URL data from mm

--- src/routes/images.js
+++ src/routes/images.js
@@ -130,7 +130,10 @@ router.post('/', async function (req, res, next) {
   //   response: mmAnswer1
   // })
   // return the URL that we get back from the mmAnswer1 (or mmAnswer2 if that fails)
-  return res.status(status).send(resultMessage)
+  return res.status(status).send({
+    message: resultMessage,
+    url: mmAnswer1 || mmAnswer2
+  })
 })
 
 module.exports = router
