c1b279fe622627d5668700ef7b988d5459b09494
Coty Condry <ccondry@cisco.com>
2020-10-26 15:30:04 -0500

    add separete POST route to create new verticals

--- src/routes/verticals.js
+++ src/routes/verticals.js
@@ -173,6 +173,37 @@ router.put('/:id', async function (req, res, next) {
     resultMessage = 'Failed to save vertical config.'
     // log it to db
     // logger.log({clientIp, host, path, url, method, operation, username, status: 500, details: resultMessage, params: req.params, response: e.message})
+  }
+})
+
+// create new vertical
+router.post('/', async function (req, res, next) {
+  const username = req.user.username
+  const operation = 'create vertical'
+  const name = req.body.name
+  console.log('user', username, 'at IP', req.clientIp, operation, name, 'requested')
+
+  req.body.owner = req.user.username
+  delete req.body._id
+
+  try {
+    // create data in the cloud mongo database
+    const response = await db.insertOne('cumulus', 'vertical', req.body)
+    console.log('user', username, 'at IP', req.clientIp, operation, 'successful', response.insertedId)
+    // update id to = _id
+    const filter = {
+      _id: db.ObjectID(response.insertedId)
+    }
+    const updates = {
+      $set: {
+        id: response.insertedId.toString()
+      }
+    }
+    await db.updateOne('cumulus', 'vertical', filter, updates)
+    // return HTTP response with new ID
+    return res.status(200).send({id: response.insertedId})
+  } catch (e) {
+    console.log('user', username, 'at IP', req.clientIp, operation, 'failed:', e.message)
     // return HTTP response
     return res.status(500).send(e.message)
   }
